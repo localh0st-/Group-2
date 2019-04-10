ffe2ad717e2baf05fecf62c6d03d7ed788d409f8
charlie <charlie.konieczny@gmail.com>
2019-04-10 17:28:56 -0400

    fixed dawits stuffs

--- code/src/main/resources/static/js/movieApp.js
+++ code/src/main/resources/static/js/movieApp.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var id= window.location.href.split("?").pop().split("=").pop();
     $.ajax({
-        url: 'http://localhost:8080/api/searchById?id=' + id,
+        url: 'http://localhost:8080/api/search?id=' + id,
         method: 'GET',
         dataType: 'json',
         success: function (data) {
