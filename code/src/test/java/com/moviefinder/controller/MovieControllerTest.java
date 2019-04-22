025dbed2f51ee5a20e0406fdb3e7f87ac8173b14
charlie <charlie.konieczny@gmail.com>
2019-04-22 16:48:12 -0400

    tests

--- code/src/test/java/com/moviefinder/controller/MovieControllerTest.java
+++ code/src/test/java/com/moviefinder/controller/MovieControllerTest.java
@@ -14,11 +14,12 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MovieController.class)
@@ -26,12 +27,11 @@ public class MovieControllerTest {
 
     @Autowired
     private MockMvc mvc;
-
     @MockBean
     private MovieService service;
 
     @Test
-    public void givenMovie_whenGetMovies_thenReturnJsonArray() throws Exception {
+    public void givenMovies_whenGetMovies_thenReturnJsonArray() throws Exception {
         Movie m = new Movie();
         m.setId(1);
         m.setTitle("foo");
@@ -43,9 +43,51 @@ public class MovieControllerTest {
         given(service.findAll()).willReturn(allMovies);
 
         mvc.perform(get("/api/movies")
-        .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", Matchers.hasSize(1)))
-        .andExpect(jsonPath("$[0].title",Matchers.is(m.getTitle())));
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].title").value(m.getTitle()))
+                .andExpect(jsonPath("$[0].id").value(m.getId()))
+                .andExpect(jsonPath("$[0].genre").value(m.getGenre()));
+    }
+
+    @Test
+    public void givenId_whenGetMovieById_thenReturnJson() throws Exception {
+        Movie m = new Movie();
+        m.setId(1);
+        m.setTitle("foo");
+
+        Optional<Movie> mov = Optional.of(m);
+
+        given(service.findById(m.getId())).willReturn(mov);
+
+        mvc.perform(get("/api/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("id", String.valueOf(m.getId())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(m.getId()))
+                .andExpect(jsonPath("$.title").value(m.getTitle()));
+    }
+
+    @Test
+    public void givenTitle_whenGetMoviesByTitle_thenReturnJsonArray() throws Exception {
+        Movie m = new Movie();
+        m.setId(2);
+        m.setTitle("foo");
+        m.setGenre("bar");
+        m.setRating("lel");
+
+        List<Movie> resultList = Arrays.asList(m);
+
+        given(service.findByTitleContaining("fo")).willReturn(resultList);
+
+        mvc.perform(get("/api/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .param("title", "fo"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].title").value(m.getTitle()))
+                .andExpect(jsonPath("$[0].id").value(m.getId()));
+
     }
 }
